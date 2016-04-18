74be644c4496f39affd6479509f76e48b1842e11
pfrisch1 <peter_frisch@brown.edu>
2016-04-17 22:42:45 -0400

    Added files via upload

--- algtesting.py
+++ algtesting.py
@@ -11,48 +11,85 @@ Created on Tue Apr 12 19:27:26 2016
 # KeyExtractor
 #MultiPitchKlapuri
 # RhythmExtractor
-#
+# Extractor
 
 
 
-
-from essentia.standard import *
+import essentia.standard as es
 from pylab import *
 from numpy import *
+from helper import *
+from sklearn import svm
+from sklearn.decomposition import PCA
 
-filename = 'audio/clyde/oranges.m4a'
+def main():
+    dim = 24
+    X = analyzeSongs(chord_vectorize,'audio/train/',dim)
+    y = ['daft']*8+['beatles']*8+['wu']*4
+    clf=svm.SVC()
+    clf.fit(X,y)
+    
+    wu = analyzeSongs(chord_vectorize,'audio/test/',dim)
+    print clf.predict(wu)
+        
 
-audio = MonoLoader(filename = filename)()
-audio = EqualLoudness()(audio)
+def chord_vectorize(audio):
+    tonal = es.TonalExtractor()
+    t = tonal(audio)
+    beatsperminute = bpm(audio)
+    return [beatsperminute]+t[1]
+    
+def hi_level_vectorize(audio):
+    tonal = es.TonalExtractor()
+    rhythm = es.RhythmExtractor()
+    dynamic = es.DynamicComplexity()
+    t = tonal(audio)
+    r = rhythm(audio)
+    d = dynamic(audio)
+    v =array([r[0],pitch2int(t[9]),majmin2int(t[10]),t[11],t[0],d[0],d[1]])
+    #v = array([r[0],t[11]])
+    return v
+    
+def lo_level_vectorize(audio):
+    ext = es.LowLevelSpectralExtractor()
+    slicer = es.Slicer(startTimes = [60],endTimes = [90])
+    audioSlice = slicer(audio)[0]
 
-rhythm = RhythmExtractor()
+    
+    
+    
+    barkbands=ext(audioSlice)[0]
+    pca = PCA(n_components=2)
+    v=pca.fit_transform(barkbands)
+    v = reshape(v,[1,-1])[0]
 
-r = rhythm(audio)
+    #dim = 2586 for two components
+    return v
+    
+    
+    
+    
+    
+    
 
-KeyExt = KeyExtractor()
-k = KeyExt(audio)
+#our info: [BPM,Key,Maj/Minor,Strength,ChangeRate]
 
-#beat = BeatTrackerMultiFeature()
-#
-#ticks,conf = beat(audio)
+####   Tonal Vector ####
+#chords_changes_rate (real) - See ChordsDescriptors algorithm documentation
+#chords_histogram (vector_real) - See ChordsDescriptors algorithm documentation
+#chords_key (string) - See ChordsDescriptors algorithm documentation
+#chords_number_rate (real) - See ChordsDescriptors algorithm documentation
+#chords_progression (vector_string) - See ChordsDetection algorithm documentation
+#chords_scale (string) - See ChordsDetection algorithm documentation
+#chords_strength (vector_real) - See ChordsDetection algorithm documentation
+#hpcp (vector_vector_real) - See HPCP algorithm documentation
+#hpcp_highres (vector_vector_real) - See HPCP algorithm documentation
+#key_key (string) - See Key algorithm documentation
+#key_scale (string) - See Key algorithm documentation
+#key_strength (real) - See Key algorithm documentation
 
-
-#multi = MultiPitchKlapuri()
-#
-#pcp = multi(audio)
-
-
-#peaks = SpectralPeaks()
-#frequencies,magnitudes = peaks(audio)
-#
-#hpcp = HPCP()
-#pcp = hpcp(frequencies,magnitudes)
-#
-#key = Key()
-#
-#out = key(pcp)
-
-
-
-#chord_detection = ChordsDetection()
-#chords = chord_detection(pcp)
+#### Rhythm Vector ####
+#bpm (real) - the tempo estimation [bpm]
+#ticks (vector_real) - the estimated tick locations [s]
+#estimates (vector_real) - the bpm estimation per frame [bpm]
+#bpmIntervals (vector_real) - list of beats interval [s]
--- helper.py
+++ helper.py
@@ -5,9 +5,20 @@ Created on Tue Apr 12 13:35:25 2016
 @author: Frisch
 """
 import os
-from essentia.standard import *
+import essentia.standard as es
 from pylab import *
 from numpy import *
+from collections import defaultdict
+
+def file2audio(path):
+    song = es.MonoLoader(filename = path)()
+    song = es.EqualLoudness()(song)
+    return song
+
+def bpm(audio):
+    rhythm = es.RhythmExtractor()
+    r = rhythm(audio)
+    return r[0]
 
 def pitchseq(pitch,conf):
     #some constants for freq2pitch:
@@ -38,21 +49,65 @@ def freq2pitch(freq,C0,name):
     n = int(h % 12)
     return name[n] + str(octave)
     
-    
-def analyzeSongs(alg):
-    songlist = os.listdir(os.getcwd()+'/audio/list')
-    #this is where I am keeping a bunch of soundfiles
-    d = {}
+def pitch2int(pitch):
+    name = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+    return name.index(pitch)
 
+def majmin2int(scale):
+    if scale == 'major':
+        return 1
+    else:
+        return 0
+    
+def analyzeSongs(alg,directory,dim):
+    songlist = os.listdir(os.getcwd()+'/'+directory)
+    for song in songlist:
+        if song[0] == '.':
+            songlist.remove(song)
+    #this is where I am keeping a bunch of soundfiles
+    l = len(songlist)
+    m = zeros([l,dim])
+    i = 0
     for songfile in songlist:
-        song = MonoLoader(filename = 'audio/list/'+songfile)()
-        song = EqualLoudness()(song)
-        d[songfile]=alg(song)
+        song = es.MonoLoader(filename = directory+songfile)()
+        song = es.EqualLoudness()(song)
+        m[i]=alg(song)
+        i += 1
+    return m
     
-    return d
     
+def chordseq(chords):
+    i = 1
+    a = chords[0]
+    seq = []
+    while i < len(chords):
+        b = chords[i]
+        if b != a:
+            seq.append(b)
+        a = b
+        i +=1
+    return seq
+    
+def countchords(chords):
+    counts = defaultdict(int)
+    for chord in chords:
+        counts[chord]+=1
+    return counts
     
 def sortDict(d):
     s = [(i,d[i]) for i in d]
     s.sort(key=lambda x: x[1], reverse=True)
     return s
+    
+def plotChords(audio):
+    tonal = es.TonalExtractor()
+    t = tonal(audio)
+    bar(arange(24),t[1])
+    
+def chordCounter(audio):
+    tonal = es.TonalExtractor()
+    t = tonal(audio)
+    chords = t[4]
+    counts = countchords(chords)
+    sortedCounts = sortDict(counts)
+    return sortedCounts
